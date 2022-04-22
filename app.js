bf07054a373aea415760c52069efdd85c8abc9f8
Luis <luisnofb@outlook.com>
2022-04-22 15:50:10 -0300

    Show

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const fetchPokemon = () => {
 
     const pokemonPromises = [];
 
-    for (let i = 1; i <= 150; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));   
+    for (let i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
     }
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
-            console.log(pokemons)
+            //console.log(pokemons)
+
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+                accumulator +=
+                    `<li class="card ${types[0]}">
+                     <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+                        <h2 class="card-title>${pokemon.id}. ${pokemon.name}</h2>
+                        <p class="card-subtitle">${types.join(' | ')}</p>
+                    </li>
+            `
+                return accumulator;
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            ul.innerHTML = lisPokemons;
         });
 }
 
